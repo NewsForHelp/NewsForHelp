ebf3959eedca008b4aee4cb88070e74792e2fce5
Your Name <you@example.com>
2015-06-05 14:18:53 +0900

    ローカルストレージ保存バグ修正

--- app-stb/js/ipconfig.js
+++ app-stb/js/ipconfig.js
@@ -1,11 +1,16 @@
 //------- IPアドレス設定画面用モジュール -------
 
+//テスト用
+var ipArray = ['192.168.109.36','192.168.109.85'];
+
+
 //IP Addressリスト
 var lsKeyIPAddressList = 'ip_address_list';
 
 //getter
 function getIPAddress(){
-  var IPAddressList = localStorage.getItem(lsKeyIPAddressList);
+  var IPAddressList = new Array();
+  IPAddressList = localStorage.getItem(lsKeyIPAddressList);
   return IPAddressList;
 }
 
@@ -13,6 +18,9 @@ function getIPAddress(){
 function setIPAddress(IPAddressList){
   localStorage.setItem(lsKeyIPAddressList, IPAddressList);
 }
+
+var configIPAddressList = new Array();
+
 
 //数値を表す文字列かどうかチェックします。
 //@return 有効な場合は true を返します。
@@ -30,16 +38,24 @@ function checkNumeric(value) {
 function checkIP(value){
   if(value > 254){
     return false;
+  }else{
+    return true;
   }
 }
 
 //入力されたIPアドレスを画面に表示します
 function loadIPAddress(){
-  for(var y=0;y<3;y++){
-    for(var x=0;x<itemXIP.lenght;x++){
-      var idx=x+''+y+'';
-      var itemXIP=ipArray[y].split('.');
-      ipItem.value = itemXIP[x];
+  var itemXIP=getIPAddress();
+  if(itemXIP.length==0){
+    itemXIP=ipArray;
+  }
+
+  for(var y=1;y<3;y++){
+    for(var x=1;x<itemXIP.lenght;x++){
+      var idx=y+''+x+'';
+      var ipItem = document.getElementById("netAdr"+idx);
+      var itemXIP=ipArray[y-1].split('.');
+      ipItem.value = itemXIP[x-1];
     }
     
   }
@@ -47,26 +63,36 @@ function loadIPAddress(){
 
 //入力されたIPアドレスを保存します
 function saveIPAddress(){
-  for(var y=0;y<3;y++){
-    var setIPAddress ='';
-    for(var x=0;x<5;x++){
-      var idx=x+''+y+'';
+  for(var y=1;y<3;y++){
+    var ipAddress ='';
+    var addFlg = true;
+    for(var x=1;x<5;x++){
+      var idx=y+''+x+'';
       var ipItem = document.getElementById("netAdr"+idx);
-      if (ipItem == "") {
-        alert('IPが不正です');
-        return false;
+      if (ipItem.value == "") {
+        console.log('IPが不正：'+idx);
+        addFlg = false;
       }else{
-        if(checkIP(ipItem)){
-          setIPAddress=setIPAddress+'.'+ipItem;
+        if(checkIP(ipItem.value)){
+          if(x==1){
+            ipAddress=ipItem.value;
+          }else{
+            ipAddress=ipAddress+'.'+ipItem.value;
+          }
         }else{
-          alert('IPが不正です');
+          console.log('IPが不正：'+idx);
+          addFlg = false;
         }
       }
     }
-    IPAddressList.add(setIPAddress);
+    if(addFlg){
+      configIPAddressList.push(ipAddress);
+    }
   }
-  ipArray = IPAddressList;
-  setIPAddress(IPAddressList);
+  if(configIPAddressList.length > 0){
+    ipArray = configIPAddressList;
+    setIPAddress(JSON.stringify(configIPAddressList));
+  }
 }  
   
 //画面遷移用
@@ -89,6 +115,15 @@ $(document).ready(function(){
 
   var ld = document.getElementById("loadIP");
   ld.addEventListener("click", loadIPAddress, false);
+  
+  var ipArrayTest=getIPAddress();
+  var hihyouji = JSON.parse(ipArrayTest);
+
+  if(hihyouji == null || hihyouji.length == 0){
+  } else {
+    loadIPAddress();
+  }
+
 });
   
   
